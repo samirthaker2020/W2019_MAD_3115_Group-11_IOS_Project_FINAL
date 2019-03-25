f4ebf33610396e486b86b6ad104a334d1dffd85d
Owner <owner@Owners-MacBook-Pro.local>
2019-03-24 22:12:25 -0400

    display user profile

--- W2019_MAD_3115_Group-11_IOS_Project_FINAL/controller/userprofile/UserProfileViewController.swift
+++ W2019_MAD_3115_Group-11_IOS_Project_FINAL/controller/userprofile/UserProfileViewController.swift
@@ -9,20 +9,52 @@
 import UIKit
 
 class UserProfileViewController: UIViewController {
+   
+    @IBOutlet weak var txtcid: UITextField!
+    
+    @IBOutlet weak var txtfname: UITextField!
+    
+    @IBOutlet weak var txtemail: UITextField!
+    
+    
+    @IBOutlet weak var txtpass: UITextField!
     var user:String?
     var c1=Customer.gblcustomer
     var custdetails1=Dictionary<String,String>()
     override func viewDidLoad() {
         super.viewDidLoad()
+       self.navigationController?.navigationBar.isHidden=false
        
-        if let u=user
-        {
-             print(c1.getcustomer(email:u))
-           
-        }
+        getuserdetails()
+        
+        
         // Do any additional setup after loading the view.
     }
     
+    func getuserdetails()  {
+        if let u=user
+        {
+            custdetails1 = c1.getcustomer(email:u)
+            
+        }
+        
+        if let fn=custdetails1["fname"]
+        {
+            txtfname.text=fn
+        }
+        if let cid=custdetails1["cid"]
+        {
+            txtcid.text=cid
+        }
+        if let email=custdetails1["email"]
+        {
+            txtemail.text=email
+        }
+        if let pass=custdetails1["pass"]
+        {
+            txtpass.text=pass
+        }
+    }
 
     /*
     // MARK: - Navigation
